140e2c9118d1c212e655e5fac89daf8c6bbb280e
Douglas Oliveira <douglas.aguiar410@gmail.com>
2022-04-27 18:11:31 -0300

    Adding the peek and match functions

---
+++ src/main/java/br/ufma/ecp/Scanner.java
@@ -0,0 +1,30 @@
+package br.ufma.ecp;
+
+public class Scanner {
+  private byte[] input;
+  private int current;
+  private int start;
+
+  public Scanner (byte[] input) {
+    this.input = input;
+    current = 0;
+    start = 0;
+  }
+
+  private void match( char c ) {
+    if ( c == peek() ) {
+      current++;
+    } else {
+      throw new Error("Syntax Error");
+    }
+  }
+
+  private char peek() {
+    if ( current < input.length ) {
+      return (char)input[current];
+    } else {
+      return 0;
+    }
+  }
+  
+}
